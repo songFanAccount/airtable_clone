241a128ad5fca9bd01e58da53f49531546da2eb7
Daniel Song <daniel.song929@gmail.com>
2025-07-22 16:26:57 +1000

    removed post

--- src/app/base/[baseID]/page.tsx
+++ src/app/base/[baseID]/page.tsx
@@ -1,12 +1,8 @@
 import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Base() {
   const session = await auth();
-
-  if (session?.user) {
-    void api.post.getLatest.prefetch();
-  }
 
   return (
     <HydrateClient>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 import { auth } from "~/server/auth";
-import { api, HydrateClient } from "~/trpc/server";
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
   const session = await auth();
-
-  if (session?.user) {
-    void api.post.getLatest.prefetch();
-  }
 
   return (
     <HydrateClient>
