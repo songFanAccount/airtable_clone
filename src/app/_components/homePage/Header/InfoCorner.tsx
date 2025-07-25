03475b25e5d3bfb910c14d565d038c4c0b66db62
Daniel Song <daniel.song929@gmail.com>
2025-07-25 23:21:27 +1000

    header button UI

--- src/app/_components/homePage/Header/InfoCorner.tsx
+++ src/app/_components/homePage/Header/InfoCorner.tsx
@@ -3,7 +3,8 @@ import ProfileButton from "./ProfileButton";
 import { useEffect, useState } from "react";
 const InfoCorner = () => {
   const [screenWidth, setScreenWidth] = useState<number>(0);
-
+  const [helpHovered, setHelpHovered] = useState<boolean>(false)
+  const [notiHovered, setNotiHovered] = useState<boolean>(false)
   useEffect(() => {
     if (typeof window === "undefined") return;
     const handleResize = () => setScreenWidth(window.innerWidth);
@@ -14,19 +15,34 @@ const InfoCorner = () => {
   return (
     <div className="flex items-center justify-end h-full w-full min-w-[170px]">
       <div className="flex flex-row items-center">
-        {
-          screenWidth < 1460
-          ?
-            <div className="flex justify-center items-center w-7 h-7">
-              <QuestionMarkCircleIcon className="w-4 h-4 flex-shrink-0"/>
-            </div>
-          :
-            <div className="flex flex-row items-center px-3">
-              <QuestionMarkCircleIcon className="w-4 h-4 flex-shrink-0"/>
-              <p className="ml-1 text-[13px]">Help</p>
-            </div>
-        }
-        <button className="flex justify-center items-center w-7 h-7 border rounded-full mx-[0.75rem]" style={{borderColor: "hsl(202, 10%, 88%)"}}>
+        <button className="rounded-full h-7 flex justify-center items-center cursor-pointer"
+          onMouseEnter={() => setHelpHovered(true)}
+          onMouseLeave={() => setHelpHovered(false)}
+          style={{
+            backgroundColor: helpHovered ? "#e5e5e5" : undefined
+          }}
+        >
+          {
+            screenWidth < 1460
+            ?
+              <div className="flex justify-center items-center w-7 h-7">
+                <QuestionMarkCircleIcon className="w-4 h-4 flex-shrink-0"/>
+              </div>
+            :
+              <div className="flex flex-row items-center px-3">
+                <QuestionMarkCircleIcon className="w-4 h-4 flex-shrink-0"/>
+                <p className="ml-1 text-[13px]">Help</p>
+              </div>
+          }
+        </button>
+        <button className="flex justify-center items-center w-7 h-7 border rounded-full mx-[0.75rem] cursor-pointer"
+          onMouseEnter={() => setNotiHovered(true)}
+          onMouseLeave={() => setNotiHovered(false)}
+          style={{
+            backgroundColor: notiHovered ? "#e5e9f0" : undefined,
+            borderColor: "hsl(202, 10%, 88%)"
+          }}
+        >
           <BellIcon className="w-4 h-4 flex-shrink-0"/>
         </button>
       </div>
--- src/app/_components/homePage/Header/SearchBar.tsx
+++ src/app/_components/homePage/Header/SearchBar.tsx
@@ -1,17 +1,29 @@
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { useState } from "react";
 
 const SearchBar = () => {
+  const [isHovered, setIsHovered] = useState<boolean>(false)
   return (
-    <div className="flex items-center justify-center px-4 rounded-[20px] border border-gray-300 h-[32px] w-full min-w-[133px] max-w-[340px]">
+    <button className="flex items-center justify-center cursor-pointer px-4 rounded-[20px] border border-gray-300 h-[32px] w-full min-w-[133px] max-w-[340px]"
+      style={{
+        borderColor: isHovered ? "#c0c1c2" : "hsl(202, 10%, 88%)",
+        boxShadow: isHovered 
+          ? 
+            "-1px -1px 0px rgba(0, 0, 0, 0.02), 1px 1px 1px rgba(0, 0, 0, 0.06), 2px 2px 2px rgba(0, 0, 0, 0.08), 3px 3px 4px rgba(0, 0, 0, 0.06)"
+          : 
+            undefined
+      }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <MagnifyingGlassIcon className="w-4 h-4 flex-shrink-0"/>
-      <input
-        id="search-airtable"
-        type="text"
-        placeholder="Search..."
-        className="flex-1 min-w-0 placeholder:text-[13px] ml-2"
-      />
+      <span
+        className="w-full flex flex-row justify-start flex-1 min-w-0 text-[13px] text-gray-500 ml-2"
+      >
+        Search...
+      </span>
       <p className="whitespace-nowrap overflow-hidden flex-shrink-0 w-auto text-[13px] text-[#979AA0]">ctrl K</p>
-    </div>
+    </button>
   );
 };
 
